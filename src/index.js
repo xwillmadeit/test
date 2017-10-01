ec927cec57328858a24282da74fc0a47df4c8f97
xwillmadeit <xwillmadeit@gmail.com>
2017-10-01 10:50:42 +0800

    update

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Modal from './Modal'
-import './modal.css'
-
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', () => {
-  const modal = new Modal()
-  modal.open()
-})
